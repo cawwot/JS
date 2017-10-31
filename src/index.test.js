e60c4028a47fd1a3cede82c55b0dada5eaa8d356
cawwot <cawwot@gmail.com>
2017-10-31 13:05:04 -0400

    stuff

--- src/index.test.js
+++ src/index.test.js
@@ -9,11 +9,11 @@ describe('a test', () => {
 });
 
 describe('index.html', () => {
-	it('should say hello', () => {
+	it('should have h1 that says users', () => {
 		const index = fs.readFileSync('./src/index.html', "utf-8");
 		jsdom.env(index,function(err, window) {
 			const h1 = window.document.getElementByTagName('h1')[0];
-			expect(h1.innerHTML).to.equal("HELLO");
+			expect(h1.innerHTML).to.equal("Users:");
 			window.close();
 		});
 	})
