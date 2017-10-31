265220740f4f53efb9d732b9a8ed6e472afcd535
cawwot <cawwot@gmail.com>
2017-10-31 09:36:35 -0400

    stuff

--- src/index.test.js
+++ src/index.test.js
@@ -19,13 +19,3 @@ describe('index.html', () => {
 	})
 })
 
-describe('index.html', () => {
-	it('should be in h1', () => {
-		const index = fs.readFileSync('.src/index.html', "utf-8");
-		jsdom.env(index,function(err, window) {
-			const h2 = window.document.getElementByTagName('h1')[0];
-			expect(h2.outerHTML).to.equal("h1");
-			window.close();
-		});
-	})
-})
