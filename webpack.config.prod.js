9c7b2f4fcafbb2160bf5dea9775ba570d0e3616c
cawwot <cawwot@gmail.com>
2017-11-08 11:38:03 -0500

    update

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
 	debug: true,
@@ -16,9 +17,28 @@ export default {
 	},
 	plugins: [
 		//Minify
-		new webpack.optimize.UglifyJsPlugin()
+		new webpack.optimize.UglifyJsPlugin(),
+
 		//Remove duplicate packages
 		new webpack.optimize.DedupePlugin(),
+
+		//create html file dynamically
+		new HtmlWebpackPlugin({
+			template: 'src/index.html',
+			minify: {
+				removeComments: true,
+				collapseWhitespace: true,
+				removeRedundantAttributes: true,
+				useShortDoctype: true,
+				removeEmptyAttributes: true,
+				removeStyleLinkTypeAttributes: true,
+				keepClosingSlash: true,
+				minifyJS: true,
+				minifyCSS: true,
+				minifyURLs: true
+			},
+			inject: true
+		})
 	],
 	module: {
 		loaders: [
