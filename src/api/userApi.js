523cf5d6ce9c0ed5a8194c7cff2a0aba037008d7
cawwot <cawwot@gmail.com>
2017-11-08 09:40:00 -0500

    update

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -7,8 +7,19 @@ export function getUsers() {
 	return get('users');
 }
 
+export function deleteUser(id) {
+	return del(`users/${id}`);
+}
+
 function get(url) {
 	return fetch(baseUrl + url).then(onSuccess, onError);
+}
+
+function del(url) {
+	const request = new Request(baseUrl + url, {
+		method: 'DELETE'
+	});
+	return fetch(request).then(onSuccess, onError);
 }
 
 function onSuccess(response) {
@@ -18,3 +29,4 @@ function onSuccess(response) {
 function onError(error) {
 	console.log(error); //eslint-disable-line no-console
 }
+
