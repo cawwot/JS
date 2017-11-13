c22682d9fe5353d37f999e04173946768ff0a64d
cawwot <cawwot@gmail.com>
2017-11-13 13:54:56 -0500

    update

--- src/index.js
+++ src/index.js
@@ -13,21 +13,21 @@ getUsers().then(result => {
 	<td>${user.lastName}</td>
 	<td>${user.email}</td>
 	</tr>`
-});
+	});
 
-global.document.getElementById('users').innerHTML = usersBody;
+	global.document.getElementById('users').innerHTML = usersBody;
 
-const deleteLinks = global.document.getElementsByClassName('deleteUser');
+	const deleteLinks = global.document.getElementsByClassName('deleteUser');
 
-Array.from(deleteLinks, link => {
-	link.onclick = function(event) {
-		const element = event.target;
-		event.preventDefault();
-		deleteUser(element.attributes["data-id"].value);
-		const row = element.parentNode.parentNode;
-		row.parentNode.removeChild(row);
-	};
+	Array.from(deleteLinks, link => {
+		link.onclick = function(event) {
+			const element = event.target;
+			event.preventDefault();
+			deleteUser(element.attributes["data-id"].value);
+			const row = element.parentNode.parentNode;
+			row.parentNode.removeChild(row);
+		};
 
-});
+	});
 });
 
